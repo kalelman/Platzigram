82a9006339aadc44787a4008e5752df6f52d660e
kalelman <erick_rojas_perez@hotmail.com>
2018-01-10 17:40:00 -0600

    Maquetando vistas LoginPlatzigram

---
+++ app/src/main/java/com/example/erickrojasperez/platzigram/view/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.erickrojasperez.platzigram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.erickrojasperez.platzigram.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
